e2f54d5d8d92d1ee19d214390557fcb4c20002dc
iyy <chengzhangiyy@163.com#macBookPro>
2018-05-03 16:59:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# lottie-web在vue中使用
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+```
+
+## 介绍
+> Lottie for Android, iOS, React Native, and Web
+https://github.com/airbnb/lottie-web
+
+用了这个库，不用设计师跟我描述动画怎么动了，AE做好甩给我JSON就够了。并且渲染在浏览器中可以是'svg' / 'canvas' / 'html' 三种可选值。
+
+本文仅介绍 `lottie-web`在vue.js中的使用，想自己动手可以参考：
+>面向前端的 Lottie & AE 动画手把手入门教学
+https://juejin.im/post/5acda0a5f265da23741153a1
+## web使用
+搭配vue.js使用,详情api使用请看lottie官网
+#### 安装依赖
+
+```
+npm install lottie-web
+//or yarn add lottie-web
+```
+#### tips
+- 不想自己设计动画可以去网站下载现成json；
+    https://www.lottiefiles.com/
+
+
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="animation-style" ref="animation"></div>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import lottie from 'lottie-web';
+import Trophy from './trophy.json'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted () {
+    this.anim = lottie.loadAnimation({
+        container: this.$refs.animation,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: Trophy,//动画json
+      }
+    )
+  }
+}
+</script>
+
+<style>
+  .animation-style{
+    width: 100px;
+    height: 100px;
+    margin: 0 auto;
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
